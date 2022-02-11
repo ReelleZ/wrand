3eadf56fa60c89d323faf3aa76abe97cd09888cc
Reelle <thefountain1993@gmail.com>
2022-02-11 16:11:11 +0900

    Update run.py

--- run.py
+++ run.py
@@ -156,7 +156,6 @@ async def h(ctx):
     try:
       await ctx.respond(embed=mkhelp())
     except discord.errors.NotFound as e:
-      await ctx.delete()
       await ctx.channel.send(embed=mkhelp())
 
 
